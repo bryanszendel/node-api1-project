9a7b36bcffef0139f4fe056776be302b289da79a
Bryan Szendel <bryan.szendel@gmail.com>
2019-08-30 16:08:47 -0600

    GET request to /api/users/:id is working

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const server = express()
 
 server.use(express.json())
 
+
+// GET all users
 server.get('/api/users', (req, res) => {
   users.find()
     .then(users => {
@@ -17,6 +19,18 @@ server.get('/api/users', (req, res) => {
     })
 })
 
+// GET specific user by id
+server.get('/api/users/:id', (req, res) => {
+  const userId = req.params.id
+  
+  users.findById(userId)
+    .then(result => {
+      res.status(200).json(result)
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'error finding the user'})
+    })
+})
 
 
 
